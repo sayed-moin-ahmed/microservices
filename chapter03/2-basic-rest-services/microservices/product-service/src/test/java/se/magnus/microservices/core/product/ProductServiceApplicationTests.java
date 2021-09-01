292957b5b1176a15220d77accc638ff05a581493
sayed <sayed.moin.85@gmail.com>
2021-09-01 22:45:16 +0530

    changes

--- chapter03/2-basic-rest-services/microservices/product-service/src/test/java/se/magnus/microservices/core/product/ProductServiceApplicationTests.java
+++ chapter03/2-basic-rest-services/microservices/product-service/src/test/java/se/magnus/microservices/core/product/ProductServiceApplicationTests.java
@@ -1,3 +1,4 @@
+/*
 package se.magnus.microservices.core.product;
 
 import org.junit.jupiter.api.Test;
@@ -11,3 +12,4 @@ class ProductServiceApplicationTests {
 	}
 
 }
+*/
--- chapter03/2-basic-rest-services/util/src/test/java/se/magnus/util/UtilApplicationTests.java
+++ chapter03/2-basic-rest-services/util/src/test/java/se/magnus/util/UtilApplicationTests.java
@@ -1,3 +1,4 @@
+/*
 package se.magnus.util;
 
 import org.junit.jupiter.api.Test;
@@ -11,3 +12,4 @@ class UtilApplicationTests {
 	}
 
 }
+*/
