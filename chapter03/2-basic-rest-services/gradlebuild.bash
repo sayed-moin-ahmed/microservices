1e4f2b8120bf70a786f5c952467c3249a6071404
sayed <sayed.moin.85@gmail.com>
2021-09-01 13:54:08 +0530

    changes

--- chapter03/2-basic-rest-services/gradlebuild.bash
+++ chapter03/2-basic-rest-services/gradlebuild.bash
@@ -4,5 +4,5 @@ cp -r microservices/product-service/gradle .
 cp microservices/product-service/gradlew .
 cp microservices/product-service/gradlew.bat .
 cp microservices/product-service/.gitignore .
-
-./gradlew build
+find microservices -depth -name "gradle" -exec rm -rfv "{}" \;
+find microservices -depth -name "gradlew*" -exec rm -fv "{}" \;
