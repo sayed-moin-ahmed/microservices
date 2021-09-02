e95b2422273f1fae8933685fe48043fc0461ddd8
sayed <sayed.moin.85@gmail.com>
2021-09-02 21:48:15 +0530

    Component scan

--- chapter03/2-basic-rest-services/microservices/product-composite-service/src/main/java/se/magnus/microservices/composite/product/ProductCompositeServiceApplication.java
+++ chapter03/2-basic-rest-services/microservices/product-composite-service/src/main/java/se/magnus/microservices/composite/product/ProductCompositeServiceApplication.java
@@ -2,8 +2,10 @@ package se.magnus.microservices.composite.product;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan("se.magnus")
 public class ProductCompositeServiceApplication {
 
 	public static void main(String[] args) {
