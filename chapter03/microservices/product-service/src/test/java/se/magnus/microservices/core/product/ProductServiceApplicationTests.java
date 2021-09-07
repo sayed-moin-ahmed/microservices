bb3575e84e69bd04ea590d66bd9020c6b8a613c4
sayed <sayed.moin.85@gmail.com>
2021-09-07 16:32:25 +0530

    refactoring

---
+++ chapter03/microservices/product-service/src/test/java/se/magnus/microservices/core/product/ProductServiceApplicationTests.java
@@ -0,0 +1,15 @@
+/*
+package se.magnus.microservices.core.product;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
+*/
---
+++ chapter03/microservices/review-service/src/test/java/se/magnus/microservices/core/review/ReviewServiceApplicationTests.java
@@ -0,0 +1,15 @@
+/*
+package se.magnus.microservices.core.review;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReviewServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
+*/
