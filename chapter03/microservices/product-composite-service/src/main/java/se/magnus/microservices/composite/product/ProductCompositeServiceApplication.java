419e6ba9f6a8293cc5bec2b3f38a105ed83e1fd0
sayed <sayed.moin.85@gmail.com>
2021-09-07 22:29:25 +0530

    refactoring

--- chapter03/microservices/product-composite-service/src/main/java/se/magnus/microservices/composite/product/ProductCompositeServiceApplication.java
+++ chapter03/microservices/product-composite-service/src/main/java/se/magnus/microservices/composite/product/ProductCompositeServiceApplication.java
@@ -1,5 +1,11 @@
 package se.magnus.microservices.composite.product;
 
+import io.swagger.v3.oas.models.ExternalDocumentation;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -8,7 +14,33 @@ import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @ComponentScan("se.magnus")
-public class ProductCompositeServiceApplication {
+public class
+ProductCompositeServiceApplication {
+	@Value("${api.common.version}") String apiVersion;
+	@Value("${api.common.title}") String apiTitle;
+	@Value("${api.common.description}") String apiDescription;
+	@Value("${api.common.termsOfServiceUrl}") String apiTermsOfService;
+	@Value("${api.common.license}") String apiLicense;
+	@Value("${api.common.licenseUrl}") String apiLicenseUrl;
+
+	@Value("${api.common.contact.name}") String apiContactName;
+	@Value("${api.common.contact.url}") String apiContactUrl;
+	@Value("${api.common.contact.email}") String apiContactEmail;
+	@Bean
+	public OpenAPI getOpenApiDocumentation() {
+		return new OpenAPI()
+				.info(new Info().title(apiTitle)
+						.description(apiDescription)
+						.version(apiVersion)
+						.contact(new Contact()
+								.name(apiContactName)
+								.url(apiContactUrl)
+								.email(apiContactEmail))
+						.termsOfService(apiTermsOfService)
+						.license(new License()
+								.name(apiLicense)
+								.url(apiLicenseUrl)));
+	}
 
 	@Bean
 	RestTemplate restTemplate() {
