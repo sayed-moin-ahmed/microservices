2eab94aee9a51f2bb706470a9888be8624c59d5f
sayed <sayed.moin.85@gmail.com>
2021-09-07 16:31:41 +0530

    refactoring

--- chapter03/2-basic-rest-services/creategradlebuild.bash
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-cp -r microservices/product-service/gradle .
-cp microservices/product-service/gradlew .
-cp microservices/product-service/gradlew.bat .
-cp microservices/product-service/.gitignore .
-find microservices -depth -name "gradle" -exec rm -rfv "{}" \;
-find microservices -depth -name "gradlew*" -exec rm -fv "{}" \;
